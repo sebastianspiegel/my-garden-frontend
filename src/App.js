1860dc412061e3ecd7de76c8112d88cec362e4d3
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-03-31 10:03:26 -0400

    update to app.js so it looks better

--- src/App.js
+++ src/App.js
@@ -12,25 +12,14 @@ class App extends Component {
   render(){
     return (
       <Router> 
-        
           <div className="App">
             <Header />
             <Switch>
-            <Route path="/seeds">
-              <SeedContainer />
-            </Route>
-            <Route exact path="/login" >
-              <LoginForm />
-            </Route>
-            <Route path="/users">
-              <UserContainer />
-            </Route> 
-            <Route path="/gardens/:id" component={GardenContainer} />
-              {/* <GardenContainer />
-            </Route> */}
+              <Route path="/seeds" container={SeedContainer} />
+              <Route path="/users" container={UserContainer} />
+              <Route path="/gardens/:id" component={GardenContainer} />
             </Switch>
           </div>
-        
       </Router>
     );
   }
